3b56dec5487a381ffa980426a3a1fdf6c0c00d7d
zmij <sergei.a.fedorov@gmail.com>
2019-01-25 22:16:33 +0300

    2. Setup include directory and tests

---
+++ include/psst/csv.hpp
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2019 Sergei A. Fedorov
+ * csv.hpp
+ *
+ *  Created on: Jan 25, 2019
+ *      Author: ser-fedorov
+ */
+
+#ifndef PSST_CSV_HPP_
+#define PSST_CSV_HPP_
+
+#endif /* PSST_CSV_HPP_ */
---
+++ test/csv_test.cpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2019 Sergei A. Fedorov
+ * csv_test.cpp
+ *
+ *  Created on: Jan 25, 2019
+ *      Author: ser-fedorov
+ */
+
+#include <psst/csv.hpp>
+
+#include <gtest/gtest.h>
+
+namespace psst::csv::test {
+
+TEST(Dummy, Dummy) {}
+
+}    // namespace psst::csv::test
